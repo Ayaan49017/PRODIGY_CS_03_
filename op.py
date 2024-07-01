054ad459eecb51917fcc8a444b273b3cbf43ad0d
Ayaan49017 <2200049017ece@gmail.com>
2024-07-01 18:06:44 +0530

    Add files via upload

---
+++ op.py
@@ -0,0 +1,11 @@
+PS C:\Users\jamee> & C:/Users/jamee/AppData/Local/Programs/Python/Python310/python.exe c:/Users/jamee/OneDrive/Desktop/ProdigyInfotech/Task-3/Task-3.py
+Enter a password to check its complexity: DOLLYtheName123123
+Password: DOLLYtheName123123
+Length: 18
+Has Uppercase: True
+Has Lowercase: True
+Has Digit: True
+Has Special Character: False
+Strength: Strong
+Feedback: Your password is strong. Password should include at least one special character (e.g., !, @, #, $, etc.).
+PS C:\Users\jamee> 
